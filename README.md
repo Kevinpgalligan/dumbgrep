1c0e7811eba65c17ba419e71f295a100a0580646
Kevin Galligan <galligankevinp@gmail.com>
2020-11-23 00:22:40 +0000

    More features and a README.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 ## dumbgrep
-🚧🚧 UNDER CONSTRUCTION 🚧🚧🚧
+A Python implementation of grep, which can be used as a template for CLI tools.
+
+Accompanying article: [Recreating grep in Python](https://kevingal.com/blog/cli-tools.html) (working title).
+
+Usage:
+
+```
+$ dumbgrep existence </tmp/war-and-peace.txt
+in this part of the house and did not even know of the existence of
+even wish to know of his existence but would now have been offended
+[...]
+$ dumbgrep -h
+usage: __init__.py [-h] [-v] [--max-count MAX_COUNT] pattern
+
+A replacement for grep.
+
+positional arguments:
+  pattern               the pattern to search for
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v                    invert matches
+  --max-count MAX_COUNT, -m MAX_COUNT
+                        max number of matches to print
+```
+
+### Installation
+Why would you want to do that, you mad, silly person?
+
+Anyway, if you really want to, clone the repository and run `python3 setup.py install`. You can then run dumbgrep from the CLI.
--- src/dumbgrepcli/__init__.py
+++ src/dumbgrepcli/__init__.py
@@ -1,15 +1,37 @@
 import argparse
 import sys
 import re
+import math
 
 def main():
     parser = argparse.ArgumentParser(description="A replacement for grep.")
     parser.add_argument("pattern", type=str, help="the pattern to search for")
+    parser.add_argument("-v", dest="invert",  default=False, action="store_true", help="invert matches")
+    parser.add_argument("--max-count", "-m", type=int, default=math.inf, help="max number of matches to print")
     args = parser.parse_args()
     regex = re.compile(args.pattern)
+    matches = 0
     for line in sys.stdin:
-        if regex.search(line):
-            sys.stdout.write(line)
+        match = regex.search(line)
+        if args.invert != bool(match):
+            matches += 1
+            try:
+                sys.stdout.write(highlight(match, line))
+            except BrokenPipeError:
+                # Next process in pipe isn't accepting input
+                # anymore, let's stop.
+                break
+        if matches >= args.max_count:
+            break
+
+def highlight(match, line):
+    if not match or not sys.stdout.isatty():
+        return line
+    return (line[:match.start()]
+        + "\033[31m" # change to red
+        + line[match.start():match.end()]
+        + "\033[0m" # reset
+        + line[match.end():])
 
 if __name__ == "__main__":
     main()
